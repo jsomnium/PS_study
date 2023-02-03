bcaf98a0a7ecb0e5cacc58e752515cec96b2ca24
Jisu <jsomnium@naver.com>
2023-02-03 14:08:36 +0900

    Update 10809_알파벳찾기.cpp

--- 10809_알파벳찾기.cpp
+++ 10809_알파벳찾기.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int arr[26]; // 알파벳 개수 카운트 배열
-    fill_n(arr,26,-1); // 값을 전부 -1로 초기화
+    fill_n(arr, 26, -1); // 값을 전부 -1로 초기화
     string S;
     cin >> S;
-    for (int i = 0; i < S.length(); i ++)
-        arr[(int)S[i]-97] = i;
-    for (int j = 0; j < 26; j ++)
+    for (int i = 0; i < S.length(); i++)
+        if (arr[(int)S[i] - 97] < 0) arr[(int)S[i] - 97] = i;
+    for (int j = 0; j < 26; j++)
         cout << arr[j] << " ";
 }
