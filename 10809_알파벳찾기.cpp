35ce477ef2187ac813f8448378f66a81401105e7
Jisu <jsomnium@naver.com>
2023-02-03 11:56:20 +0900

    Create 10809_알파벳찾기.cpp

---
+++ 10809_알파벳찾기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[26]; // 알파벳 개수 카운트 배열
+    fill_n(arr,26,-1); // 값을 전부 -1로 초기화
+    string S;
+    cin >> S;
+    for (int i = 0; i < S.length(); i ++)
+        arr[(int)S[i]-97] = i;
+    for (int j = 0; j < 26; j ++)
+        cout << arr[j] << " ";
+}
