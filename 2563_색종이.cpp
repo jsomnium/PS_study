d12fbadf43d93b95ff2c6176eb80281b83332626
Jisu <92006284+jsomnium@users.noreply.github.com>
2023-08-01 15:29:19 +0900

    [ADD] 2563번 문제 풀이

--- 2563_색종이.cpp
+++ 2563_색종이.cpp
@@ -2,17 +2,22 @@
 // 유형: 구현
 // 난이도: 실버5
 
-// 색종이 하나의 넓이는 100이다.
-// 겹치는 경우의 넓이를 빼주면 답을 구할 수 있다.
-
-
-#include <iostream>
+#include<iostream>
 using namespace std;
-
+int a[100][100];
 int main(){
-    int num, x, y; // 붙인 색종이의 수, 위치
-    cin >> num;
-    for (int i = 0; i < num; i++;){
-        cin >> x >> y;
+    int n,x,y,cnt=0;
+    cin>>n;
+    while(n--){
+        cin>>x>>y;
+        for(int i=y;i<y+10;i++){
+            for(int j=x;j<x+10;j++){
+                if(!a[i][j]){
+                    cnt++;
+                    a[i][j]=1;
+                }
+            }
+        }
     }
+    cout<<cnt;
 }
