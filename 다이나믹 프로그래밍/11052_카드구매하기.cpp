b9097da77f1f699c7e9793abbac80093ca92e0d2
Jisu <92006284+jsomnium@users.noreply.github.com>
2023-08-11 15:48:57 +0900

    [ADD] 11052번 문제풀이

---
+++ 다이나믹 프로그래밍/11052_카드구매하기.cpp
@@ -0,0 +1,35 @@
+// 문제: 카드 구매하기
+// 유형: 다이나믹 프로그래밍
+// 난이도: 실버1
+
+#include <iostream>
+using namespace std;
+
+int max(int a, int b){
+    return a > b ? a : b;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N; // 구매하고자 하는 카드의 개수
+    cin >> N;
+    int cost[10001]; // cost[i] : 카드 i팩의 가격
+    int DP[10001]; // DP[i] : i개 카드를 구매할 때의 최대 비용
+
+    for (int i = 1; i <= N; i++)
+        cin >> cost[i];
+
+    DP[0] = 0;
+    cost[0] = 0;
+
+    for (int i = 1; i <= N; i++){ // 총 i개의 카드를 구매하려고 할 때,
+        for (int j = 1; j <= i; j++){ // 마지막에 j팩 카드를 한 장 산다.
+            DP[i] = max(DP[i], DP[i-j] + cost[j]); // 뭐가 더 큰지 비교
+        }
+    }
+
+    cout << DP[N];
+}
