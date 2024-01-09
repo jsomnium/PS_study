78b42e8775b86ac8a756587d492c432ac8b4c983
Jisu <92006284+jsomnium@users.noreply.github.com>
2024-01-09 15:52:49 +0900

    42583번: 문제 풀이✨

--- 큐/PS_42583_다리를지나는트럭.cpp
+++ 큐/PS_42583_다리를지나는트럭.cpp
@@ -1,34 +1,38 @@
-#include <string>
-#include <queue>
-#include <vector>
-using namespace std;
-
-int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int Answer = 0; // 경과 시간
-    queue<int> WaitingTruck;
-    queue<pair<int,int>> AcrossingTruck; // 다리를 지나는 중인 트럭
-    int onBridgeWeight = 0; // 다리를 지나는 중인 트럭의 무게 합
-    int onBridgeCount = 0; // 다리를 지나는 중인 트럭의 개수
-
-    for (int i = 0; i < truck_weights.size(); i++){
-        WaitingTruck.push(truck_weights[i]);
-    }
-
-    do {
-        Answer++;
-        if (!AcrossingTruck.empty() && AcrossingTruck.front().first >= bridge_length){
-            onBridgeWeight -= AcrossingTruck.front().second;
-            AcrossingTruck.pop();
-        }
-        if (onBridgeCount < bridge_length && onBridgeWeight + WaitingTruck.front() <= weight)
-        {
-            onBridgeCount++;
-            onBridgeWeight += WaitingTruck.front();
-            WaitingTruck.pop();
-            AcrossingTruck.front().first += 1;
-            AcrossingTruck.push(make_pair(0,WaitingTruck.front()));
-        }
-    } while (!AcrossingTruck.empty());
-
-    return Answer;
+#include <string>
+#include <vector>
+#include <queue>
+ 
+using namespace std;
+ 
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = 0;
+    int next_truck = 0;     // 대기 트럭 중 맨 앞의 트럭의 인덱스
+    int weight_sum = 0;     // 다리 위에 있는 트럭의 무게 합
+    queue<int> on_bridge;   // 다리 위에 있는 트럭
+ 
+    // 다리 위에 있을 수 있는 트럭의 수 만큼 미리 넣어둠
+    for (int i = 0; i < bridge_length; i++)
+        on_bridge.push(0);
+ 
+    while (!on_bridge.empty()) {
+        answer++;
+        weight_sum -= on_bridge.front();
+        on_bridge.pop();
+ 
+        // 대기 트럭이 있는 경우
+        if (next_truck < truck_weights.size()) {
+            // 다리 무게 조건 만족시, 트럭 push
+            if (weight_sum + truck_weights[next_truck] <= weight) {
+                weight_sum += truck_weights[next_truck];
+                on_bridge.push(truck_weights[next_truck]);
+                next_truck++;
+            }
+            // 만족 X시, 0 push
+            else {
+                on_bridge.push(0);
+            }
+        }
+    }
+ 
+    return answer;
 }
