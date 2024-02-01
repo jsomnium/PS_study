5e0bf4ba9221e913cd5083b4a3fe58881b42098a
Jisu <92006284+jsomnium@users.noreply.github.com>
2024-02-01 14:24:17 +0900

    42747번: 문제 풀이✨

---
+++ 정렬 알고리즘/PS_42747_H-index.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.rbegin(), citations.rend()); // 내림차순 정렬
+
+    for(int i = 0; i < citations.size(); i++){
+        if(citations[i] >= i+1){
+            answer = i + 1;
+        }
+    }
+
+    return answer;
+}
