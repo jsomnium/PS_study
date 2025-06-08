bc0806bbc30dc58a26f5e8c6baa14d3c9f24c6ab
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-06-09 00:03:34 +0900

    10039번: 문제 풀이✨

---
+++ [BJ] 10039_평균점수.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	
+	int total = 0;
+	
+	for(int i = 0; i < 5; i++){
+    	int n = 0;
+    	cin >> n;
+    
+    	if(n < 40) n = 40;
+    	total += n;
+    }
+	
+    cout << total / 5;
+ 
+}
