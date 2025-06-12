bd0446f98dc34c0df9b022b822f915541efc22ed
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-06-12 15:47:33 +0900

    12945번: 문제 풀이✨

---
+++ [PS] 12945_피보나치.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int fib[100001] = {0, 1};  // fib[0] = 0, fib[1] = 1
+
+    for (int i = 2; i <= n; i++) {
+        fib[i] = (fib[i-1] + fib[i-2]) % 1234567;
+    }
+
+    return fib[n];
+}
