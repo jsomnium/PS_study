8438944af8437f15007a5aa8e5fbe594c268d4dc
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-07-01 22:45:50 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -1,19 +1,23 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer = 0;
+    sort(citations.begin(), citations.end());
     
-    sort(citations.begin(), citations.end(), greater<int>()); // 내림차순 정렬
+    int n = citations.size();
+    int h = 0;
     
-    for(int i = 0; i < citations.size(); i++){
-        if (citations[i] >= i + 1){
-            answer = i + 1;
-        } else break;
+    for (int i = 0; i < n; i++) {
+        int possibleH = n - i;
+        if (citations[i] >= possibleH) {
+            h = possibleH;
+            break;
+        }
     }
     
-    return answer;
+    return h;
 }
