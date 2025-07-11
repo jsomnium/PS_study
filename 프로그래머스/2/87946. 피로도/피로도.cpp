fce63e028cd9cab8b7edfc36fbedf4f1164e0642
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-07-11 16:16:14 +0900

    [level 2] Title: 피로도, Time: 0.80 ms, Memory: 4.24 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,19 +1,18 @@
 #include <string>
 #include <vector>
-#define MAX 8
 
 using namespace std;
 
 int answer = 0;
-bool visited[MAX] = {0};
+bool visited[8] = {0};
 
-void dfs(int cnt, int k, vector<vector<int>> &dungeons) {
+void dfs(int cnt, int k, vector<vector<int>> &dungeons){
     if (cnt > answer) answer = cnt;
     
-    for (int i=0; i<dungeons.size(); i++) {
-        if (!visited[i] && dungeons[i][0] <= k) {
+    for (int i = 0; i < dungeons.size(); i++){
+        if (!visited[i] && dungeons[i][0] <= k){
             visited[i] = true;
-            dfs(cnt + 1, k - dungeons[i][1], dungeons);
+            dfs (cnt + 1, k - dungeons[i][1], dungeons);
             visited[i] = false;
         }
     }
