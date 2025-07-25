3c23ceaf98d8256d4fce61504cbf7dc2b7006282
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-07-25 22:38:31 +0900

    [level 3] Title: N으로 표현, Time: 8.74 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -1,39 +1,41 @@
 #include <string>
 #include <vector>
 #include <set>
-#include <iostream>
+
 using namespace std;
 
 int solution(int N, int number) {
     if (N == number) return 1;
     vector<set<int>> dp(9);
     
-    dp[1].insert(N);
-    
-    // (55, 555 등) 미리 추가
-    int concat = N;
-    for (int i = 2; i <= 8; i++) {
-        concat = concat * 10 + N;
-        dp[i].insert(concat);
+    // 5, 55 와 같은 연산 없는 숫자 구하기
+    int originalNum = N;
+    for (int i = 2; i <= 8; i++){
+        originalNum = originalNum * 10 + N;
+        dp[i].insert(originalNum);
     }
     
-    for (int i = 2; i <= 8; i++) {
-        for (int j = 1; j < i; j++) {
-            for (int a : dp[i-j]) {
-                for (int b : dp[j]) {
+    // 시작점
+    dp[1].insert(N);
+    
+    // dp 점화식 연산
+    for (int i = 2; i <= 8; i++){
+        for (int j = 1; j < i; j++){
+            for (int a : dp[i-j]){
+                for (int b : dp[j]){
                     dp[i].insert(a + b);
                     dp[i].insert(a - b);
                     dp[i].insert(a * b);
-                    if (b != 0 && a % b == 0)  // b로 나눴을 때 나머지 0일 때만
+                    if (b != 0 && a % b == 0)
                         dp[i].insert(a / b);
-                    if (a != 0 && b % a == 0)  // a로 나눌 수도 있으니
-                        dp[i].insert(b / a);
                 }
             }
         }
         
+        // 목표 숫자를 찾으면 바로 return
         if (dp[i].count(number)) return i;
     }
     
+    // 못 찾을 시 -1
     return -1;
 }
