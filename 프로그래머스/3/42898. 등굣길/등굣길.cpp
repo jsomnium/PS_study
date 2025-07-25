0f245ac84251050912cedf2bed3e511cf47840e8
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-07-25 22:42:04 +0900

    [level 3] Title: 등굣길, Time: 0.06 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -4,33 +4,29 @@
 using namespace std;
 
 int solution(int m, int n, vector<vector<int>> puddles) {
-    const int MOD = 1000000007;
+    // 가로가 m, 세로가 n 이면 열이 m개, 행이 n개이다
+    vector<vector<int>> dp(n+1, vector(m+1, 0)); // m x n 배열
     
-    // 세로(n) x 가로(m) 배열 생성
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    // 집 (시작점)
     dp[1][1] = 1;
-
-    // 웅덩이 위치를 미리 표시
-    for (auto p : puddles) {
-        int x = p[0];
-        int y = p[1];
-        dp[y][x] = -1; // 웅덩이는 -1로 표시
+    
+    // 웅덩이
+    for (auto p : puddles){
+        dp[p[1]][p[0]] = -1;
     }
-
-    // dp 채우기
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
+    
+    // dp 구하기
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            if (i == 1 && j == 1) continue;
             if (dp[i][j] == -1) {
-                dp[i][j] = 0; // 웅덩이 다시 0으로
+                dp[i][j] = 0;
                 continue;
             }
-
-            if (i > 1)
-                dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
-            if (j > 1)
-                dp[i][j] = (dp[i][j] + dp[i][j - 1]) % MOD;
+            
+            dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007;
         }
     }
-
+    
     return dp[n][m];
 }
