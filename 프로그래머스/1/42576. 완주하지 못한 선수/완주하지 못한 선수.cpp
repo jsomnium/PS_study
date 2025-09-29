006b4695a357d3a693ee381e90dff3a40e3da7a0
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-09-29 18:48:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 108.91 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,29 +1,21 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    string answer = "";
+    map<string, int> m;
     
-    // 1. 정렬
-    sort(participant.begin(),participant.end());
-    sort(completion.begin(),completion.end());
-    
-    // 2. 정렬해서 비교
-    for (int i = 0; i < participant.size(); i++){
-        if (i == participant.size() - 1){
-            // 정렬 기준 마지막 사람이 completition에 없는 경우에는 배열 길이 때문에 체크 못하므로 따로 if문으로 빼주기
-            answer = participant[i];
-            break;
-        }
-        else if (participant[i] != completion[i]){
-            // 비교한 값이 없다면 정답
-            answer = participant[i];
-            break;
-        }
+    for (auto p : participant){
+        m[p]++;
     }
     
-    return answer;
+    for (auto c : completion){
+        m[c]--;
+    }
+    
+    for (auto pair : m){
+        if (pair.second > 0) return pair.first;
+    }
 }
