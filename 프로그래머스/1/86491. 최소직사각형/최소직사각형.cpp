d505f969cc93b5988671be4ecade8b3432ceaa62
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-10-02 14:44:31 +0900

    [level 1] Title: 최소직사각형, Time: 0.91 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -1,14 +1,15 @@
 #include <string>
 #include <vector>
+
 using namespace std;
 
 int solution(vector<vector<int>> sizes) {
     int maxRow = 0;
     int maxCol = 0;
     
-    for (auto s : sizes){
-        maxRow = max(maxRow, max(s[0], s[1]));
-        maxCol = max(maxCol, min(s[0], s[1]));
+    for (auto size : sizes){
+        maxRow = max(maxRow, max(size[0], size[1]));
+        maxCol = max(maxCol, min(size[0], size[1]));
     }
     
     return maxRow * maxCol;
