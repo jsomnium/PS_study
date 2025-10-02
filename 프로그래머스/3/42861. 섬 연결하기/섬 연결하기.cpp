834e9743b409484ee25dd532abd5929140a3606c
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-10-02 14:44:47 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,38 +1,48 @@
-#include <algorithm>
-#include <string>
-#include <vector>
- 
-using namespace std;
- 
-int set[101];   // node의 parent를 저장하는 배열
- 
-int getParent(int node) {
-    if(set[node] == node) return node;
-    return set[node] = getParent(set[node]);
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool comp (vector<int>&a, vector<int> b){
+    return a[2] < b[2];
 }
- 
-bool cmp(vector<int> a, vector<int> b) {
-    return a[2] < b[2];
-}
- 
-int solution(int n, vector<vector<int>> costs) {
-    int answer = 0;
- 
-    for(int i = 0; i < n; i++)
-        set[i] = i;
-    
-    sort(costs.begin(), costs.end(), cmp);  // 비용을 오름차순 정렬
- 
-    for(int i = 0; i < costs.size(); i++) {
-        int start = getParent(costs[i][0]);
-        int end = getParent(costs[i][1]);
-        int cost = costs[i][2];
- 
-        if(start != end) {  // cycle이 만들어지지 않을 경우 다리를 추가
-            answer += cost;
-            set[end] = start;
-        }
-    }
- 
-    return answer;
+
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0;
+    int check = 0;
+    
+    sort (costs.begin(), costs.end(), comp);
+    
+    int parent[n];
+    for (int i = 0; i < n; ++i){
+        parent[i] = i;
+    }
+    
+    for (int i = 0; i < costs.size(); ++i){
+        if (parent[costs[i][0]] != parent[costs[i][1]]){
+            if(parent[costs[i][0]] > parent[costs[i][1]]){
+                int bigtemp = parent[costs[i][0]];
+                int smalltemp = parent[costs[i][1]];
+                for (int j = 0; j < n; ++j){
+                    if (parent[j] == bigtemp){
+                        parent[j] = smalltemp;
+                    }
+                }
+            } else {
+                int bigtemp = parent[costs[i][1]];
+                int smalltemp = parent[costs[i][0]];
+                for (int j = 0; j < n; ++j){
+                    if (parent[j] == bigtemp){
+                        parent[j] = smalltemp;
+                    }
+                }
+            }
+            answer += costs[i][2];
+            ++check;
+        }
+        if (check == n-1)
+            break;
+    }
+    
+    return answer;
 }
