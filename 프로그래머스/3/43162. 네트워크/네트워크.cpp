80a73c0b15c214714e10242cd2bf788c7d258f48
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-11-10 20:10:48 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,30 +1,41 @@
 #include <string>
 #include <vector>
+#include <queue> // std::queue 사용
 
 using namespace std;
 
-bool isVisited[201];
-
-void DFS(int cur, int n, vector<vector<int>> com){
-    isVisited[cur] = true;
+void bfs(int start_node, int n, vector<vector<int>>& computers, vector<bool>& visited_nodes) {
+    queue<int> q;
+    q.push(start_node);
+    visited_nodes[start_node] = true;
     
-    for (int i = 0; i < n; i++){
-        if(!isVisited[i] && com[cur][i] == 1){
-            DFS(i, n, com);
+    while (!q.empty()) {
+        int current_node = q.front();
+        q.pop();
+        
+        for (int neighbor_node = 0; neighbor_node < n; ++neighbor_node) {
+            // 1. 자기 자신(current_node == neighbor_node)이 아니고
+            // 2. 연결되어 있고 (computers[current_node][neighbor_node] == 1)
+            // 3. 아직 방문하지 않았다면 (!visited_nodes[neighbor_node])
+            if (current_node != neighbor_node && computers[current_node][neighbor_node] == 1 && !visited_nodes[neighbor_node]) {
+                q.push(neighbor_node);         // 큐에 추가
+                visited_nodes[neighbor_node] = true; // 방문 처리
+            }
         }
     }
 }
 
 int solution(int n, vector<vector<int>> computers) {
-    int answer = 0;
+    int network_count = 0;
+    vector<bool> visited_nodes(n, false); 
     
-    // 방문 체크만 잘 세면 되는 문제
-    for (int i = 0; i < n; i++){
-        if (!isVisited[i]){
-            DFS(i, n, computers);
-            answer++;
+    // 모든 컴퓨터를 순회하며 아직 방문하지 않은 컴퓨터를 찾음
+    for (int i = 0; i < n; ++i) {
+        if (!visited_nodes[i]) {
+            bfs(i, n, computers, visited_nodes);
+            network_count++;
         }
     }
     
-    return answer;
+    return network_count;
 }
