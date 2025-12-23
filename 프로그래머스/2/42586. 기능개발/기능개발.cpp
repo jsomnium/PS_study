091bfb3f083c349bf7c14dc2d69ea776cfb98c1f
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-12-23 17:17:16 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,50 +1,35 @@
 #include <string>
-#include <vector>
 #include <queue>
-#include <iostream>
+#include <vector>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    queue<int> workDate;
     vector<int> answer;
-    queue<int> q;
-    
-    // progresses를 100에서 빼고, speed로 나누면 몫이 남은 일수이다.
-    // 나머지가 있는 경우에는, +1
-    // 만약, 93이 들어오면 (100-93 = 7)이므로 7이 남은 작업 일수이다. [7]
-    // 만약, 30이 들어오면 (100-30 = 70) 이므로 (70/30 = 2 이고 나머지 10) 따라서 3일이 남은 작업 일수이다. [3]
-    // 만약, 55가 들어오면 44 / 5 = 8에 나머지까지 생각하면 9일이다. [8]
-    
-    // 배포되어야 하는 순서대로 배포되어야 한다.
-    // 따라서 먼저 들어간 게 먼저 나와야 하므로 선입선출 queue 사용
     
     for (int i = 0; i < progresses.size(); i++){
-        int day = 0;
-        day = 100 - progresses[i];
-        cout << "day : " << day << endl;
-        
-        if (day % speeds[i] == 0){
-            day = day/speeds[i];
-        } else {
-            day = day/speeds[i] + 1;
+        int leftProgress = 100 - progresses[i];
+        int leftDate = 0;
+        if (speeds[i] != 0){
+            if (leftProgress % speeds[i] == 0) leftDate = leftProgress / speeds[i];
+            else leftDate = leftProgress / speeds[i] + 1;
         }
         
-        q.push(day);
-        
-        cout << i << "st final day : " << day << endl;
+        workDate.push(leftDate);
     }
     
-    while (!q.empty()){
-        int front = q.front();
-        q.pop();
-        int count = 1;
+    while(!workDate.empty()){
+        int cnt = 1;
+        int front = workDate.front();
+        workDate.pop();
         
-        while (!q.empty() && front >= q.front()){
-            q.pop();
-            count++;
+        while (!workDate.empty() && front >= workDate.front()){
+            workDate.pop();
+            cnt++;
         }
         
-        answer.push_back(count);
+        answer.push_back(cnt);
     }
     
     return answer;
