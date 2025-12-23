c37f476dc9bc1cc8d7355d8c41918744a00d2077
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-12-23 15:44:21 +0900

    [level 2] Title: 전화번호 목록, Time: 80.62 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=cpp) 
 
 ### 성능 요약
 
-메모리: 35.6 MB, 시간: 77.46 ms
+메모리: 35.6 MB, 시간: 80.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 24일 09:56:49
+2025년 12월 23일 15:44:19
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -5,14 +5,18 @@
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    // 정렬해서 인접하는 것 끼리 동일한 숫자가 있는지 체크하면 된다
+    bool answer = true;
+    
     sort(phone_book.begin(), phone_book.end());
     
+    // 인접하는 두 배열이 동일한지 체크하면 됨
+    // 배열에서 필요한 만큼을 추출하는 방법이 substr
     for (int i = 0; i < phone_book.size() - 1; i++){
         if (phone_book[i] == phone_book[i+1].substr(0, phone_book[i].size())){
-            return false;
+            answer = false;
+            break;
         }
     }
     
-    return true;
+    return answer;
 }
