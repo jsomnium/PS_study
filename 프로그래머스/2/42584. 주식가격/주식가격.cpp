fdd50c582c5545e738772f0f8d75f4138ebd7df6
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-12-24 13:07:24 +0900

    [level 2] Title: 주식가격, Time: 29.01 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,23 +1,32 @@
-#include <iostream>
+#include <string>
 #include <vector>
-#include <stack>;
+#include <stack>
+
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    stack<int> st;
-    int size = prices.size();
-    vector<int> answer(size);
+    vector<int> answer(prices.size()); // 벡터 0으로 초기화
+    stack<int> st; // 인덱스를 저장한다
     
-    for (int i = 0; i < size; i++){
-        while (!st.empty() && prices[i] < prices[st.top()]){
-            answer[st.top()] = i - st.top();
+    for (int index = 0; index < prices.size(); index++){
+        if (st.empty()) {
+            st.push(index);
+            continue;
+        }
+        
+        // 주식 가격이 떨어질 시
+        while (!st.empty() && prices[st.top()] > prices[index]){
+            answer[st.top()] = index - st.top();
             st.pop();
         }
-        st.push(i);
+        
+        st.push(index);
     }
-    while (!st.empty()) {
-        answer[st.top()] = size - st.top() - 1;
+    
+    while (!st.empty()){
+        answer[st.top()] = prices.size() - st.top() - 1;
         st.pop();
     }
+    
     return answer;
 }
