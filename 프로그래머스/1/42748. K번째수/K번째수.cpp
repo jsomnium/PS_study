309b0680b4eecff5345d23340bf8433bb3e110c4
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-12-25 23:18:04 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
@@ -11,11 +10,10 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
         vector<int> result;
         int k = command[2] - 1;
         
-        // 새로운 배열 만들기
         for (int i = command[0]-1; i <= command[1]-1; i++)
             result.push_back(array[i]);
         
-        sort(result.begin(), result.end()); // 오름차순 정렬
+        sort(result.begin(), result.end());
         
         // K번째 수 찾기
         answer.push_back(result[k]);
