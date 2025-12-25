41a83e78bc2c6b6186b050eae401b679000f3b2c
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-12-25 23:28:40 +0900

    [level 2] Title: 가장 큰 수, Time: 143.57 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -4,27 +4,30 @@
 
 using namespace std;
 
-bool comp(string s1, string s2){
-    int temp1 = stoi(s1 + s2);
-    int temp2 = stoi(s2 + s1);
+bool comp (string a, string b){
+    int bigA = stoi(a + b);
+    int bigB = stoi(b + a);
     
-    return temp1 > temp2;
+    return bigA > bigB;
 }
 
 string solution(vector<int> numbers) {
     string answer = "";
     vector<string> nums;
-    
-    for (int num: numbers)
-        nums.push_back(to_string(num));
 
+    for (int num : numbers){
+        nums.push_back(to_string(num));
+    }
+    
     sort(nums.begin(), nums.end(), comp);
     
-    for(string num: nums)
-        answer += num;
+    for (auto n: nums){
+        answer += n;
+    }
     
-    if(answer[0] == '0')
-        answer = "0";
+    if (answer[0] == '0'){
+        answer = '0';
+    }
     
     return answer;
 }
