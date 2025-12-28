b7cbafcb22959fb8bbb417849d4f19c8c2a166e7
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-12-28 23:36:27 +0900

    [level 2] Title: 구명보트, Time: 1.82 ms, Memory: 4.78 MB -BaekjoonHub
    Implement solution function to optimize boat usage.

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// 가장 무거운 사람먼저 채우고 남는 무게를 가장 작은 사람으로 메꾼다
 int solution(vector<int> people, int limit) {
     sort(people.begin(), people.end(), greater<int>()); // 내림차순
     int highIdx = 0; // 가장 무거운 사람의 인덱스
