d571689768dda241b71f939a431450acfff20adb
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-01-07 11:19:34 +0900

    [level 2] Title: 타겟 넘버, Time: 74.94 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,23 +3,20 @@
 
 using namespace std;
 
-void DFS(int& count, int targetCount, int target, int index, int number, vector<int> numbers){
-    if (index == targetCount){
-        if (number == target) count++;
-        
+void dfs(int index, int& target, int& count, int sum, vector<int> numbers){
+    if (index == numbers.size()){
+        if (sum == target) count++;
         return;
     }
     
-    DFS(count, targetCount, target, index + 1, number + numbers[index], numbers);
-    DFS(count, targetCount, target, index + 1, number - numbers[index], numbers);
+    dfs(index + 1, target, count, sum + numbers[index], numbers);
+    dfs(index + 1, target, count, sum - numbers[index], numbers);
 }
 
 int solution(vector<int> numbers, int target) {
     int answer = 0;
     
-    int targetCount = numbers.size();
-    
-    DFS(answer, targetCount, target, 0, 0, numbers);
+    dfs(0, target, answer, 0, numbers);
     
     return answer;
 }
