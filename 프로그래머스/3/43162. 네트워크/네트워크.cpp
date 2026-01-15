3ecc195a2ea4b850e3936ebeb6d60dfff3d209c0
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-01-15 18:00:30 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,41 +1,38 @@
 #include <string>
 #include <vector>
-#include <queue> // std::queue 사용
-
+#include <queue>
 using namespace std;
 
-void bfs(int start_node, int n, vector<vector<int>>& computers, vector<bool>& visited_nodes) {
+int cnt = 0;
+
+void bfs(int node, vector<vector<int>> computers, vector<bool>& visited){
     queue<int> q;
-    q.push(start_node);
-    visited_nodes[start_node] = true;
-    
-    while (!q.empty()) {
-        int current_node = q.front();
+    q.push(node);
+    visited[node] = true;
+
+    while (!q.empty()){
+        int front = q.front();
         q.pop();
         
-        for (int neighbor_node = 0; neighbor_node < n; ++neighbor_node) {
-            // 1. 자기 자신(current_node == neighbor_node)이 아니고
-            // 2. 연결되어 있고 (computers[current_node][neighbor_node] == 1)
-            // 3. 아직 방문하지 않았다면 (!visited_nodes[neighbor_node])
-            if (current_node != neighbor_node && computers[current_node][neighbor_node] == 1 && !visited_nodes[neighbor_node]) {
-                q.push(neighbor_node);         // 큐에 추가
-                visited_nodes[neighbor_node] = true; // 방문 처리
+        // 연결할 수 있는 것을 다음 큐에 담기
+        for (int i = 0; i < computers.size(); i++){
+            if (computers[front][i] == 1 && front != i && !visited[i]){
+                q.push(i);
+                visited[i] = true;
             }
         }
     }
 }
 
 int solution(int n, vector<vector<int>> computers) {
-    int network_count = 0;
-    vector<bool> visited_nodes(n, false); 
+    vector<bool> visited(n, false); // 방문 여부 확인
     
-    // 모든 컴퓨터를 순회하며 아직 방문하지 않은 컴퓨터를 찾음
-    for (int i = 0; i < n; ++i) {
-        if (!visited_nodes[i]) {
-            bfs(i, n, computers, visited_nodes);
-            network_count++;
-        }
+    for (int i = 0; i < n; i++){
+        if (visited[i]) continue;
+        
+        bfs(i, computers, visited);
+        cnt++;
     }
     
-    return network_count;
+    return cnt;
 }
