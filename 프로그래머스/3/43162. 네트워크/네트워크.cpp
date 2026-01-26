08ea4a3f235ad3bb194b6cc8db3e2f026ead3d83
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-01-26 16:19:53 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -3,36 +3,44 @@
 #include <queue>
 using namespace std;
 
-int cnt = 0;
+vector<bool> visited(201, false);
 
-void bfs(int node, vector<vector<int>> computers, vector<bool>& visited){
+int bfs(int net, vector<vector<int>> graph){
     queue<int> q;
-    q.push(node);
-    visited[node] = true;
-
+    q.push(net);
+    
     while (!q.empty()){
         int front = q.front();
         q.pop();
         
-        // 연결할 수 있는 것을 다음 큐에 담기
-        for (int i = 0; i < computers.size(); i++){
-            if (computers[front][i] == 1 && front != i && !visited[i]){
-                q.push(i);
-                visited[i] = true;
-            }
+        for (int computer : graph[front]){
+            if (visited[computer]) continue;
+            q.push(computer);
+            visited[computer] = true;
         }
     }
+    
+    return 1;
 }
 
 int solution(int n, vector<vector<int>> computers) {
-    vector<bool> visited(n, false); // 방문 여부 확인
+    int answer = 0;
+    vector<vector<int>> graph(n);
     
+    // 인접그래프 생성
     for (int i = 0; i < n; i++){
-        if (visited[i]) continue;
-        
-        bfs(i, computers, visited);
-        cnt++;
+        for (int j = 0; j < n; j++){
+            if (i == j) continue;
+            if (computers[i][j] == 1) graph[i].push_back(j);
+        }
     }
     
-    return cnt;
+    // 모든 점을 방문할 때 까지 진행
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i]) continue;
+        answer += bfs(i, graph);
+    }
+    
+    return answer;
 }
