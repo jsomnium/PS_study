40f680032683c9e04cb8d24183504b0f26f1657e
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-01-27 18:22:43 +0900

    [level 3] Title: 여행경로, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,42 +1,47 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <algorithm>
+#include <map> // 맵 헤더 추가
+
 using namespace std;
 
-map<string, vector<pair<string, bool>>> adj;
 int totalTickets = 0;
 
-bool dfs(string airport, vector<string>& answer){
-    answer.push_back(airport);
-    
+bool dfs(string current, map<string, vector<pair<string, bool>>>& adj, vector<string>& answer) {
+    answer.push_back(current);
+
     if (answer.size() == totalTickets + 1) return true;
-    
-    for (auto& vec : adj[airport]){
-        string dest = vec.first;
-        bool& isUsed = vec.second;
-        
-        if (isUsed == false){
-            isUsed = true;
-            
-            if (dfs(dest, answer)) return true;
-            
-            isUsed = false;
-            answer.pop_back();
-        }
+
+    for (auto& ticket : adj[current]) {
+        string nextAirport = ticket.first;
+        bool& isUsed = ticket.second;
+
+        if (isUsed) continue;
+
+        isUsed = true;
+        if (dfs(nextAirport, adj, answer)) return true;
+
+        // 백트래킹
+        isUsed = false;
+        answer.pop_back();
     }
     return false;
 }
 
 vector<string> solution(vector<vector<string>> tickets) {
     vector<string> answer;
+    map<string, vector<pair<string, bool>>> adj; // 꺾쇠괄호 사용
     totalTickets = tickets.size();
-    
-    for (auto t : tickets) adj[t[0]].push_back(make_pair(t[1], false));
-    
-    for (auto& a : adj) sort(a.second.begin(), a.second.end());
-    
-    dfs("ICN", answer);
-    
+
+    for (auto& t : tickets) {
+        adj[t[0]].push_back({t[1], false});
+    }
+
+    // 알파벳 순서가 앞서는 경로를 찾기 위해 정렬
+    for (auto& pair : adj) {
+        sort(pair.second.begin(), pair.second.end());
+    }
+
+    dfs("ICN", adj, answer);
     return answer;
 }
