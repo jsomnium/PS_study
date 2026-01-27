95f9149b383d6a672b032fda6e02d970dd256b3f
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-01-27 17:01:38 +0900

    [level 3] Title: 아이템 줍기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/README.md
+++ 프로그래머스/3/87694. 아이템 줍기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아이템 줍기 - 87694 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.06 ms
+메모리: 4.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 13일 12:22:39
+2026년 01월 27일 17:01:35
 
 ### 문제 설명
 
--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -3,65 +3,64 @@
 #include <queue>
 using namespace std;
 
-constexpr int MAX = 102;
-int map[MAX][MAX] = {0};
-int dist[MAX][MAX] = {-1};
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
 
-int dx[] = {1, -1, 0, 0};
-int dy[] = {0, 0, 1, -1};
+// 전역 변수 초기화
+vector<vector<int>> board(102, vector<int>(102, 0));
+vector<vector<int>> dist(102, vector<int>(102, -1));
 
-// 2. bfs에서 dist 파라미터 제거
-void bfs(int characterX, int characterY, int itemX, int itemY, int &answer){
+int bfs(int startX, int startY, int targetX, int targetY){
     queue<pair<int, int>> q;
-    q.push({characterX, characterY});
+    q.push({startX, startY});
+    dist[startY][startX] = 0; // 시작점 거리 초기화 (y, x 순서 주의)
     
-    dist[characterY][characterX] = 0; 
-
     while (!q.empty()){
-        int cx = q.front().first;
-        int cy = q.front().second;
+        int curX = q.front().first;
+        int curY = q.front().second;
         q.pop();
         
-        if (cx == itemX && cy == itemY) {
-            answer = (dist[cy][cx]) / 2;
-            break; 
+        if (curX == targetX && curY == targetY){
+            return dist[curY][curX] / 2; // 세미콜론 추가 및 2배 확장했으므로 2로 나눔
         }
         
-        for (int move = 0; move < 4; move++){
-            int nextX = cx + dx[move];
-            int nextY = cy + dy[move];
+        for (int i = 0; i < 4; i++){
+            int nx = curX + dx[i];
+            int ny = curY + dy[i];
             
-            if (nextX < 0 || nextX >= MAX || nextY < 0 || nextY >= MAX) continue;
+            if (nx < 0 || ny < 0 || nx >= 102 || ny >= 102) continue;
             
-            if (map[nextY][nextX] == 1 && dist[nextY][nextX] == 0){
-                dist[nextY][nextX] = dist[cy][cx] + 1; 
-                q.push({nextX, nextY});
+            // 테두리(1)이고 방문하지 않은(-1) 경우만 이동
+            if (board[ny][nx] == 1 && dist[ny][nx] == -1){
+                dist[ny][nx] = dist[curY][curX] + 1;
+                q.push({nx, ny});
             }
         }
     }
+    return -1;
 }
 
 int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
-    int answer = 0;
-    characterX *= 2, characterY *= 2, itemX *= 2, itemY *= 2;
-
+    // 좌표 2배 확장
+    characterX *= 2; characterY *= 2; itemX *= 2; itemY *= 2;
+    
     for (auto r : rectangle){
-        for (int y = 2 * r[1]; y <= 2 * r[3]; y++){
-            for (int x = 2 * r[0]; x <= 2 * r[2]; x++){
-                map[y][x] = 1;
+        int x1 = r[0] * 2, y1 = r[1] * 2, x2 = r[2] * 2, y2 = r[3] * 2;
+        for (int i = y1; i <= y2; i++){
+            for (int j = x1; j <= x2; j++){
+                board[i][j] = 1; // 일단 직사각형 영역을 모두 1로 채움
             }
         }
     }
     
     for (auto r : rectangle){
-        for (int y = 2 * r[1] + 1; y < 2 * r[3]; y++){
-            for (int x = 2 * r[0] + 1; x < 2 * r[2]; x++){
-                map[y][x] = 0;
+        int x1 = r[0] * 2, y1 = r[1] * 2, x2 = r[2] * 2, y2 = r[3] * 2;
+        for (int i = y1 + 1; i < y2; i++){
+            for (int j = x1 + 1; j < x2; j++){
+                board[i][j] = 0; // 내부는 다시 0으로 비워 테두리만 남김
             }
         }
     }
     
-    bfs(characterX, characterY, itemX, itemY, answer);
-    
-    return answer;
+    return bfs(characterX, characterY, itemX, itemY);
 }
