c62c4d4a68cae56d857b47c69db1bd7410765058
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-02-03 14:08:56 +0900

    [level 3] Title: N으로 표현, Time: 10.03 ms, Memory: 4.25 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -4,34 +4,33 @@
 using namespace std;
 
 int solution(int N, int number) {
-    vector <set<int>> dp(9);
-    dp[1].insert(N);
+    vector<set<int>> DP(9);
+    DP[1].insert(N);
+    // DP[i] = N을 i회 사용해서 만들 수 있는 수들의 배열
+    // 즉, DP[1] = { N }
+    // DP[2] = { 'NN', DP[1](연산)DP[1] }
+    // DP[3] = { 'NNN', DP[1](연산)DP[2], DP[2](연산)DP[2] }
     
-    // 같은 수 일 경우 바로 return하기
-    if (N == number) return 1; 
+    if (N == number) return 1;
     
-    // 2 이상부터는 N 사용 횟수 카운트로 dp식 세워서 목표 number 찾기
-    
-    // 5, 55, 555, 5555 와 같은 연속 숫자 포함
-    int originalNum = N;
-    for (int i = 2; i <= 8; i++){
-        originalNum = 10 * originalNum + N;
-        dp[i].insert(originalNum);
+    int origin_number = N;
+    for (int i = 2; i < 9; ++i){
+        origin_number = origin_number * 10 + N;
+        DP[i].insert(origin_number);
     }
     
-    // 모든 연산 후 숫자 찾기
-    for (int i = 2; i <= 8; i++){
-        for (int j = 1; j < i; j++){
-            for (int a : dp[i - j]){
-                for (int b : dp [j]){
-                    dp[i].insert(a + b);
-                    dp[i].insert(a - b);
-                    dp[i].insert(a * b);
-                    if (b != 0 && a % b == 0) dp[i].insert(a / b);
+    for (int i = 2; i < 9; ++i){
+        for (int j = 1; j < i; ++j){
+            for (int a : DP[i - j]){
+                for (int b : DP[j]){
+                    DP[i].insert(a + b);
+                    DP[i].insert(a - b);
+                    DP[i].insert(a * b);
+                    if (b != 0 && a % b == 0) DP[i].insert(a / b);
                 }
             }
         }
-    if(dp[i].count(number)) return i; // 최솟값을 찾는 것이므로 바로 return
+        if (DP[i].count(number)) return i;
     }
     
     return -1;
