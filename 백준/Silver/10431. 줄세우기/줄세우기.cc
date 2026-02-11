1a88a3ea9f2d1a465e79df3048a6543b0b9939a4
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-02-11 16:42:13 +0900

    [Silver V] Title: 줄세우기, Time: 36 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.cc
+++ 백준/Silver/10431. 줄세우기/줄세우기.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 #include <set>
 #include <iostream>
@@ -5,38 +6,28 @@
 using namespace std;
 
 int main(){
-    int P; // 테스트 케이스의 수
+    int P;
     cin >> P;
     
     while (P--){
         int testCase = 0;
         int totalSteps = 0;
+        
         vector<int> Student(20, 0);
-        set<int> Sort;
-    
-        // 테스트케이스 입력받기
+        set<int> Height;
+        
         cin >> testCase;
-        for (int i = 0; i < 20; i++){
-            cin >> Student[i];
+        for (int i = 0; i < 20; i++) cin >> Student[i];
+        
+        for (int j = 0; j < 20; j++){
+            int curStudent = Student[j];
+            
+            auto it = Height.lower_bound(curStudent);
+            totalSteps += distance (it, Height.end());
+            
+            Height.insert(curStudent);
         }
-    
-        // i번째 학생을 줄세우려고 할 때,
-        // 자신보다 키가 큰 학생이 있으면 줄을 다시 세워야 한다.
-        // 즉, 자신보다 키가 큰 학생이 처음 나오는 위치를 확인하면 된다.
-        for (int i = 0; i < 20; i++) {
-            int curStudent = Student[i];
-
-            // lower_bound로 나보다 키가 크거나 같은 첫 번째 사람 찾기
-            auto it = Sort.lower_bound(curStudent);
-
-            // it부터 끝(end)까지의 거리가 바로 '나보다 키 큰 사람의 수'
-            // 즉, 한 발씩 물러나야 하는 학생들의 수입니다.
-            totalSteps += distance(it, Sort.end());
-
-            // 이제 줄에 서기
-            Sort.insert(curStudent);
-        }
-    
+        
         cout << testCase << " " << totalSteps << endl;
     }
     
