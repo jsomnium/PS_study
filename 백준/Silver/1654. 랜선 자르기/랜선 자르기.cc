4ca77098b2c26fe574af73475522cd7fbca0a613
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-03-18 18:08:34 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,59 +1,46 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-unsigned int ans;
-unsigned int N,K;
-unsigned int list[10000];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-int main()
-{
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(NULL);
+    int K, N;
+    cin >> K >> N;
 
-	cin >> K >> N;
-    
-	unsigned int maxi = 0;
-    
-	for (int i = 0; i < K; i++)
-	{
-		cin >> list[i];
-		maxi = max(maxi, list[i]);
-	}
+    vector<long long> lan(K);
+    long long max_len = 0;
+    for (int i = 0; i < K; i++) {
+        cin >> lan[i];
+        if (lan[i] > max_len) max_len = lan[i];
+    }
 
-	unsigned int left = 1, right = maxi, mid;
-	
-	while (left <= right)
-	{
-		// mid 연산
-		mid = (left + right) / 2;
-        
-		// 몫의 합을 저장하는 변수
-		unsigned int now = 0; 
-        
-		for (int i = 0; i < K; i++)
-		{
-			//mid로 나눈 몫을 저장
-			now += list[i] / mid;
-		}
+    long long low = 1;
+    long long high = max_len;
+    long long answer = 0;
 
-		if (now >= N)
-		{
-			// 현재 mid로 나눈 값이 N보다 크거나 같다면,
-			// left를 움직여 길이가 더 긴 경우는 가능한지 검사
-			left = mid + 1;
-            
-			// N개를 만들 수 있을 때, 답을 더 큰 값으로 계속 갱신
-			ans = max(ans, mid);
-		}
-		else
-		{
-			// 현재 mid로 나눈 값이 N보다 작다면,
-			// right 움직여 길이가 더 짧은 경우는 가능한지 검사
-			right = mid - 1;
-		}
-	}
-	
-	cout << ans << '\n';
+    while (low <= high) {
+        long long mid = low + (high - low) / 2;
+        long long cnt = 0;
+
+        for (int i = 0; i < K; i++) {
+            cnt += (lan[i] / mid);
+        }
+
+        if (cnt >= N) {
+            // N개 이상 만들 수 있다면?
+            // 현재 mid는 정답 후보! 더 큰 길이를 찾아보자
+            answer = mid;
+            low = mid + 1;
+        } else {
+            // N개도 못 만든다면? 너무 길게 자른 것! 길이를 줄이자.
+            high = mid - 1;
+        }
+    }
+
+    cout << answer;
+    return 0;
 }
