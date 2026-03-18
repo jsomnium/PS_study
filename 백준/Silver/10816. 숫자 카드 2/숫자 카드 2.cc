1c16a9d7bf9ac9e6c71cc88f148a3b500e2fd16f
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-03-18 10:44:44 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 316 ms, Memory: 8296 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -2,27 +2,53 @@
 #include <vector>
 #include <algorithm>
 
-int main()
-{
-	std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
+using namespace std;
 
-	int N, M, n;
-	std::vector<int> v;
+int main(){
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	vector<int> ans;
+	int N, M;
 
-	std::cin >> N;
-	while (N--)
-	{
-		std::cin >> n;
-		v.push_back(n);
+	cin >> N;
+	vector<int> arr;
+	while (N--){
+		int num;
+		cin >> num;
+		arr.push_back(num);
 	}
 
-	std::sort(v.begin(), v.end());
+	sort(arr.begin(), arr.end());
 
-	std::cin >> M;
-	while (M--)
-	{
-		std::cin >> n;
-		std::cout << std::upper_bound(v.begin(), v.end(), n)
-			- std::lower_bound(v.begin(), v.end(), n) << ' ';
+	cin >> M;
+	while (M--){
+		int target;
+		cin >> target;
+
+		int low = -1;
+		int high_start = arr.size();
+		while (low + 1 < high_start){
+			int mid = low + (high_start - low) / 2;
+			if (arr[mid] >= target){
+				high_start = mid;
+			} else {
+				low = mid;
+			}
+		}
+
+		low = -1;
+		int high_end = arr.size();
+		while (low + 1 < high_end){
+			int mid = low + (high_end - low) / 2;
+			if (arr[mid] > target){
+				high_end = mid;
+			} else {
+				low = mid;
+			}
+		}
+
+		ans.push_back(high_end - high_start);
 	}
+
+	for (auto a : ans)
+		cout << a << " ";
 }
