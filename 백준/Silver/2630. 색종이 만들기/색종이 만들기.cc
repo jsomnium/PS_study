7b07a13cda2cc6cea453e13242ca2bb3fbd459e6
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-04-14 23:25:08 +0900

    [Silver II] Title: 색종이 만들기, Time: 4 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,58 +1,50 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-int N;
-int paper[128][128];
+int arr[129][129];
 int white = 0;
 int blue = 0;
 
-// 해당 영역이 모두 같은 색인지 체크하는 함수
-bool is_uniform(int y, int x, int size) {
-    int color = paper[y][x];
-    for (int i = y; i < y + size; i++) {
-        for (int j = x; j < x + size; j++) {
-            if (paper[i][j] != color) {
-                return false;
+void fc(int x, int y, int k){
+    bool cur = false;
+    int first_color = arr[x][y];
+    
+    for (int i = x; i < x + k; i++){
+        for (int j = y; j < y + k; j++){
+            if (arr[i][j] != first_color){
+                cur = true;
+                break;
             }
         }
     }
-    return true;
-}
-
-// 분할 정복 함수
-void solve(int y, int x, int size) {
-    // 1. 현재 영역이 모두 같은 색인가?
-    if (is_uniform(y, x, size)) {
-        if (paper[y][x] == 0) white++;
-        else blue++;
-        return;
+    
+    if (cur){
+        fc(x, y, k / 2);
+        fc(x, y + k / 2, k / 2);
+        fc(x + k / 2, y, k / 2);
+        fc(x + k / 2, y + k / 2, k / 2);
+    } else {
+        if (first_color == 1)
+            blue++;
+        else
+            white++;
     }
-
-    // 2. 다른 색이 섞여 있다면 4등분으로 쪼개기
-    int next_size = size / 2;
-    solve(y, x, next_size);                         // 왼쪽 위
-    solve(y, x + next_size, next_size);             // 오른쪽 위
-    solve(y + next_size, x, next_size);             // 왼쪽 아래
-    solve(y + next_size, x + next_size, next_size); // 오른쪽 아래
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> paper[i][j];
+int main(){
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cin >> arr[i][j];
         }
     }
-
-    solve(0, 0, N);
-
-    cout << white << "\n";
-    cout << blue << "\n";
-
+    
+    fc(0, 0, n);
+    
+    cout << white << '\n';
+    cout << blue << '\n';
+    
     return 0;
 }
