3f9cee141884c0f0e9d547e8076aa9de3658140e
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-05-06 15:18:18 +0900

    [level 3] Title: 이중우선순위큐, Time: 51.86 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,13 +1,13 @@
 #include <string>
 #include <vector>
-#include <set> // multiset은 set 헤더에 있습니다
+#include <set>
 #include <sstream>
 
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
     vector<int> answer;
-    multiset<int> ms; // 오름차순 자동 정렬되는 컨테이너
+    multiset<int> ms;
 
     for(string op : operations){
         stringstream ss(op);
@@ -16,16 +16,13 @@ vector<int> solution(vector<string> operations) {
         ss >> cmd >> num;
 
         if (cmd == "I"){
-            ms.insert(num); // 삽입 (자동 정렬됨)
+            ms.insert(num);
         }
         else if (cmd == "D" && !ms.empty()){
             if(num == 1){
-                // 최댓값 삭제: 맨 뒤 요소(end 바로 앞) 삭제
-                // --ms.end() 혹은 prev(ms.end()) 사용
                 ms.erase(prev(ms.end())); 
             }
             else if(num == -1){
-                // 최솟값 삭제: 맨 앞 요소(begin) 삭제
                 ms.erase(ms.begin());
             }
         }
