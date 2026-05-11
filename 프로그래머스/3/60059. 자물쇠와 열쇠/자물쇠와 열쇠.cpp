065f555fd851830e029c6d89747d757ec2bda2a5
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-05-11 22:56:46 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.61 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
@@ -3,44 +3,40 @@
 
 using namespace std;
 
-// 시계방향 90도 회전 코드
-vector<vector<int>> rotate90(vector<vector<int>>& arr){
-    int row = arr.size(), col = arr[0].size();
+vector<vector<int>> rotate90 (vector<vector<int>>& arr){
+    int row = arr.size();
+    int col = arr[0].size();
     
     vector<vector<int>> rotatedArr(col, vector<int>(row, 0));
     
     for (int i = 0; i < row; ++i){
         for (int j = 0; j < col; ++j)
-            rotatedArr[j][row - 1 - i] = arr[i][j];
+            rotatedArr[j][row-1-i] = arr[i][j];
     }
     
     return rotatedArr;
 }
 
-// 자물쇠의 영역 내에서는 열쇠의 돌기 부분과 자물쇠의 홈 부분이 일치해야 한다
-// 열쇠의 돌기와 자물쇠의 돌기가 만나서는 안된다
-// 자물쇠의 모든 홈을 열쇠의 돌기로 채워야만 한다
 bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
-    // 90도 회전 경우 모두 고려하여 후보에 담기
     vector<vector<vector<int>>> candidates;
     vector<vector<int>> newArr(key);
+    
     for (int rotate = 0; rotate < 4; ++rotate){
         candidates.push_back(newArr);
         newArr = rotate90(newArr);
     }
     
-    // Lock의 채워야 하는 부분 구하기
     int lockCnt = 0;
     for (auto r : lock){
-        for (auto c : r){
+        for (auto c : r)
             if (c == 0) lockCnt++;
-        }
     }
     
-    // 탐색 시작
     for (auto candidate : candidates){
-        int maxRowKey = candidate.size(), maxColKey = candidate[0].size();
-        int maxRowLock = lock.size(), maxColLock = lock[0].size();
+        int maxRowKey = candidate.size();
+        int maxColKey = candidate[0].size();
+        int maxRowLock = lock.size();
+        int maxColLock = lock[0].size();
         
         for (int dr = -maxRowKey + 1; dr < maxRowLock; ++dr){
             for (int dc = -maxColKey + 1; dc < maxColLock; ++dc){
@@ -62,7 +58,6 @@ bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
                 if (!isConflict && currentCnt == lockCnt) return true;
             }
         }
-        
     }
     
     return false;
