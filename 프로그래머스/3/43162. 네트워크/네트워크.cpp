2bf8d6e3c8836f1ea417a180918796f7a6dce7ac
Jisu <92006284+jsomnium@users.noreply.github.com>
2026-05-13 16:18:22 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -3,44 +3,37 @@
 #include <queue>
 using namespace std;
 
-vector<bool> visited(201, false);
-
-int bfs(int net, vector<vector<int>> graph){
-    queue<int> q;
-    q.push(net);
-    
-    while (!q.empty()){
-        int front = q.front();
-        q.pop();
-        
-        for (int computer : graph[front]){
-            if (visited[computer]) continue;
-            q.push(computer);
-            visited[computer] = true;
-        }
-    }
-    
-    return 1;
-}
-
 int solution(int n, vector<vector<int>> computers) {
-    int answer = 0;
-    vector<vector<int>> graph(n);
+    int networkCnt = 0;
     
-    // 인접그래프 생성
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            if (i == j) continue;
-            if (computers[i][j] == 1) graph[i].push_back(j);
-        }
-    }
-    
-    // 모든 점을 방문할 때 까지 진행
+    vector<vector<int>> adj(n);
     for (int i = 0; i < n; i++)
-    {
+        for (int j = 0; j < n; j++)
+            if (i != j && computers[i][j] == 1)
+                adj[i].push_back(j);
+    
+    vector<bool> visited(n, false);
+    queue<int> q;
+    
+    for (int i = 0; i < n; i++){
         if (visited[i]) continue;
-        answer += bfs(i, graph);
+        
+        q.push(i);
+        visited[i] = true;
+        
+        while (!q.empty()){
+            int front = q.front();
+            q.pop();
+            
+            for (auto a : adj[front]){
+                if (visited[a]) continue;
+                visited[a] = true;
+                q.push(a);
+            }
+        }
+        
+        networkCnt++;
     }
     
-    return answer;
+    return networkCnt;
 }
